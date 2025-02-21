de73e35be5acf188819deed14193edb220250cb1
hyewonimdang <hyehyekuk202@gmail.com>
2025-02-21 16:58:19 +0900

    feat: 캘린더 기능 및 UI 구현

---
+++ lib/screen/voice_detection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VoiceDetection extends StatelessWidget {
+  const VoiceDetection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
